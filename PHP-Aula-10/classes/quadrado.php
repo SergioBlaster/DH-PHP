8d1d65b6789dfd82c3ffd5e7d6c7b87fc7113b16
Sergio Gomes <sergioblaster@gmail.com>
2019-07-17 22:44:01 -0300

    Subindo aula 10

---
+++ PHP-Aula-10/classes/quadrado.php
@@ -0,0 +1,13 @@
+<?php
+class Quadrado
+{
+
+    public $ladoA;
+    public $ladoB;
+
+    public function calculaArea()
+    {
+        $area = ($this->ladoA * $this->ladoB);
+        return $area;
+    }
+}
---
+++ PHP-Aula-10/quadrado_form.php
@@ -0,0 +1,59 @@
+<?php
+require_once("classes/quadrado.php");
+
+if ($_POST) {
+    $ladoA = $_REQUEST["ladoA"];
+    $ladoB = $_REQUEST["ladoB"];
+
+    $area = new Quadrado();
+    $area->ladoA = $ladoA;
+    $area->ladoB = $ladoB;
+
+    $resultado = $area->calculaArea();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Digital House</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <form class="" action="" method="post">
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <div class="col-md-4">
+                    <label for="ladoA">Lado A</label>
+                    <input type="text" class="form-control" name="ladoA" id="ladoA">
+                </div>
+            </div>
+            <div class="form-group col-md-12">
+                <div class="col-md-4">
+                    <label for="ladoB">Lado B</label>
+                    <input type="text" class="form-control" name="ladoB" id="ladoB">
+                </div>
+            </div>
+            <div class="form-group col-md-12">
+                <div class="col-md-2">
+                    <button type="submit" class="form-control btn btn-primary">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <?php if ($resultado) : ?>
+        <div class="col-md-12">
+            <div class="col-md-2 alert alert-success">
+                <p><?= "A área do quadrado é: " . $resultado ?></p>
+            </div>
+        </div>
+    <?php endif; ?>
+</body>
+
+</html>
